db37c854e8ea95051e5c577be6d4a9ae6720311d
iam-an <naktsu1059@gmail>
2025-10-18 10:06:25 +0900

    add: write setting to config.json

--- namai/src/run.py
+++ namai/src/run.py
@@ -12,23 +12,31 @@ with open(CONFIG, "r", encoding="utf-8") as f:
 company = settings["COMPANY"]
 start = settings["START_DAY"]
 end = settings["END_DAY"]
+stl_target = settings["STL_TARGET"]
+pred_target = settings["PRED_TARGET"]
+pred_feature = settings["PRED_FEATURE"]
+average = settings["AVERAGE"]
+average_upper = settings["AVERAGE_UPPER"]
+average_lower = settings["AVERAGE_LOWER"]
+period = settings["PERIOD"]
+output_name_png = settings["OUTPUT_NAME_PNG"]
 
 def main():
     df = get_yfinace(company_name=company, start=start, end=end)
     train, test = split_train_test(df=df, test_size=0.2)
-    df_train_stl = use_stl(target = "High", df_train=train, period=24)
+    df_train_stl = use_stl(target = stl_target, df_train=train, period=period)
     y_pred_test, test, train = make_model_prophet(train=df_train_stl, test=test, target="High", date_col="Date")
-    evaluate_score(test=test["y"], y_pred_test=y_pred_test["yhat"])
+    evaluate_score(test=test[pred_target], y_pred_test=y_pred_test[average])
     make_plot_time(
-        ds_train=train["ds"],
-        y_train=train["y"],
-        ds_test=test["ds"],
-        y_test=test["y"],
-        y_pred_test=y_pred_test["yhat"],
-        y_pred_high=y_pred_test["yhat_upper"],
-        y_pred_low=y_pred_test["yhat_lower"],
+        ds_train=train[pred_feature],
+        y_train=train[pred_target],
+        ds_test=test[pred_feature],
+        y_test=test[pred_target],
+        y_pred_test=y_pred_test[average],
+        y_pred_high=y_pred_test[average_upper],
+        y_pred_low=y_pred_test[average_lower],
         output=OUTPUT,
-        fig_name="time.png"
+        fig_name=output_name_png
         )
 
 if __name__ == "__main__":
