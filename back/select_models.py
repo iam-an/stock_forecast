f3de17044a0408e23b49a94c11e8bcf2ffc17feb
iam-an <naktsu1059@gmail>
2026-03-26 07:37:31 +0900

    WIP: とりあえず動く

--- back/select_models.py
+++ back/select_models.py
@@ -16,34 +16,40 @@ from namai.src.utils.constant import MODELS, COMPANIES
 # pred_range = "1week"
 # model_type = "LR"
 
-def select_models(company, pred_range, model_type):
+def load_model(company, pred_range, model_type):
     # select model
     model_path = MODELS / company / f"{pred_range}_{model_type}.joblib"
-    print(model_path)
     try:
         model = joblib.load(model_path)
     except:
         with open(model_path, "rb") as f:
             model = cloudpickle.load(f)
+    assert model is not None, "modelがありません"
+    return model
+
+def select_models(company, pred_range, model_type):
 
     # 期間決定
     if pred_range == "1day":
         hist_period = "1mo"
         hist_inter = "1d"
+        window = 7
     elif pred_range == "1week":
         hist_period = "1y"
         hist_inter = "1d"
+        window = 14
     elif pred_range == "1month":
         hist_period = "1y"
         hist_inter = "1d"
+        window = 30
     elif pred_range == "1year":
         hist_period = "3y"
         hist_inter = "1d"
+        window = 90
     # 略称の取得
     with open(COMPANIES / "ticker_master.yaml", "r", encoding="utf-8") as f:
         ticker_master = yaml.safe_load(f)
     ticker = ticker_master[company]
-    print(ticker)
     # common process independ on period
     # data cleaning
     df = yf.download(ticker, period=hist_period, interval=hist_inter, threads=False)
@@ -56,48 +62,64 @@ def select_models(company, pred_range, model_type):
     df = df.rename({f"('Low', '{ticker}')" : "Low"})
     df = df.rename({f"('Close', '{ticker}')" : "Close"})
     df = df.drop("Open", "Low", "Close")
+    """make display data"""
+    indata = df["High"].to_numpy()
+    act_data = indata[-window:]
 
     if pred_range == "1day" or pred_range == "1week":
-        print("enter if")
+        model = load_model(company, pred_range, model_type)
 
         min_date = df.select(pl.col("Date").min()).to_series()[0]
         df = df.with_columns((df["Date"]-min_date).alias("Times"))
         df = df.with_columns(df["Times"].dt.total_microseconds()/ (24*60*60*1_000_000))
-        indata = df["High"].to_numpy()
 
         """use model to predict"""
         pred_data = np.append(indata[-1:] , model(indata))
-
-        """make display data"""
-        act_data = indata[-15:]
-        act_date = df["Date"][-15:].dt.strftime("%m/%d")
+        act_date = df["Date"][-window:].dt.strftime("%m/%d")
         base_date = df["Date"][-1]
         pred_date = [base_date]
-        d = base_date
+
         while len(pred_date) < len(pred_data):
-            d = d + timedelta(days=1)
-            if d.weekday() < 5 and not jpholiday.is_holiday(d):
-                pred_date.append(d)
-        pred_date = [d.strftime("%m/%d") for d in pred_date]
+            base_date = base_date + timedelta(days=1)
+            if base_date.weekday() < 5 and not jpholiday.is_holiday(base_date):
+                pred_date.append(base_date)
+        pred_date = [base_date.strftime("%m/%d") for base_date in pred_date]
     
     elif pred_range == "1month" or pred_range == "1year":
-        print("enter")
-        from namai.src.core.use_yfinance import get_yfinace
-        from namai.src.core.split_data import split_train_test
+        if pred_range == "1month":
+            period = 30
+        elif pred_range == "1year":
+            period = 365
         from namai.src.core.make_model import make_model_prophet
-        from namai.src.core.analysis import evaluate_score, set_mlflow, save_artifacts, write_to_json
-        from namai.src.core.make_plot import make_plot_time
         from namai.src.core.discompose_data import use_stl
-        from namai.src.core.rename import rename_for_prophet
-        df = rename_for_prophet(df, rename_map)
         df = df.rename({"Date": "ds"})
         df = df.rename({"High": "y"})
-        print(hist_period)
-        df_stl = use_stl(target = "y", df=df, period=hist_period)
-        train = df_stl.to_pandas()
-        model_fitted = make_model_prophet(train)
-        save_artifacts(model=model_fitted, artifact_path=OUTPUT, company=company)
-        
-        pass
+        real_data = df.to_pandas()
+        model = make_model_prophet(real_data)
+        future = model.make_future_dataframe(periods=period)
+        pred_data = model.predict(future)
+        last_date = real_data["ds"].max()
+
+        # 営業日 + 未来のみ抽出
+        filtered_dates = []
+        filtered_values = []
+
+        for i in range(len(pred_data)):
+            date = pred_data["ds"].iloc[i]
+            if date > last_date:
+                if date.weekday() < 5 and not jpholiday.is_holiday(date):
+                    filtered_dates.append(date)  # 👈 文字列やめる
+                    filtered_values.append(pred_data["yhat"].iloc[i])
+
+        pred_date = filtered_dates
+        pred_data = filtered_values
+
+        act_date = df["ds"][-window:].to_pandas().tolist()
+        act_data = df["y"][-window:].to_list()
+        if len(act_date) > 0 and len(pred_date) > 0:
+            pred_date = [act_date[-1]] + pred_date
+            pred_data = [act_data[-1]] + pred_data
 
     return act_date, act_data, pred_date, pred_data
+        
+
--- each_workspace/namai/src/core/make_model.py
+++ each_workspace/namai/src/core/make_model.py
@@ -4,6 +4,5 @@ import numpy as np
 
 def make_model_prophet(train):
     model = Prophet(growth="linear", changepoint_prior_scale=0.3, n_changepoints=50)
-    train["y"] = np.log(train["Close"])
     model.fit(train)
     return model
